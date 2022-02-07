7c743fc39c9c8e4c9f0a188564e3a20a0b219c65
Abhinav Setia <abhinav.setia@sky.uk>
2022-02-07 05:28:35 +0000

    intial commit

---
+++ containers/predictions/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Define the main route
+@app.get('/')
+def root_route():
+  return { 'error': 'Use GET /prediction instead of the root route!' }
+
+
+# Define the heartbeat
+@app.get('/hearbeat')
+def heartbeat():
+  return { 'error': 'Use GET /prediction instead of the root route!' }
