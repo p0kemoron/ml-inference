80b754c202177bb882f28e092fb6a7cc379ad846
Abhinav Setia <bits.abhinav@gmail.com>
2022-02-08 06:11:43 +0000

    update compose

--- handler/workers.py
+++ handler/workers.py
@@ -2,16 +2,16 @@ import os
 from celery import Celery
 from ml.score_reports import ScoreReportsDummyModel
 
-app = Celery()
+cel_app = Celery()
 
-app.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
-app.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
+cel_app.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
+cel_app.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
 
-@app.task(ignore_result=False)
+@cel_app.task(ignore_result=False)
 def create_task():
     return True
 
-@app.task(ignore_result=False)
+@cel_app.task(ignore_result=False)
 def get_score():
     model = ScoreReportsDummyModel()
     return model.predict()
