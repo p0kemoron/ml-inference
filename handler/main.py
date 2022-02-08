284910a588aa0b9e77b9263b9ea7b412156084bb
Abhinav Setia <bits.abhinav@gmail.com>
2022-02-08 09:12:53 +0000

    add fetch tasks

--- handler/main.py
+++ handler/main.py
@@ -19,7 +19,7 @@ def heartbeat():
 @app.post('/submit')
 async def get_task_id(data: List[RequestBody], status_code=201, response_model=SubmittedTask):
   pred_data = get_pred_df()
-  task = create_task.delay()
+  task = get_score.delay()
   return JSONResponse({"task_id":task.id, "status": str(task.status)})
 
 
@@ -29,4 +29,4 @@ async def get_report_score(task_id):
   if not task.ready():
     return JSONResponse(status_code=201, content={'task_id': str(task_id), 'status': 'Processing'})
 
-  return {"task_id": str(task_id), "status": str(task.status), "score": float(task.result)}
+  return {"task_id": str(task_id), "status": str(task.status), "score": task.result}
